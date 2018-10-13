1069ba1004f21479764fdbc26546f5fa9c918f7f
kkanbam <suy.suy.lee@gmail.com>
2018-10-13 20:10:31 +0900

    Add BOJ 1934

---
+++ BOJ/1934.cpp
@@ -0,0 +1,39 @@
+/*
+	20181013
+	1934. 최소공배수
+*/
+#include <iostream>
+
+using namespace std;
+
+int gcd((nt a, int b);
+int lcm(int a, int b);
+
+int main()
+{
+    int tc, a, b;
+
+    cin >> tc;
+    
+    for (int i = 0; i < tc; i++) {
+        cin >> a >> b;
+        cout << lcm(a, b) << endl;
+    }
+    
+    return 0;
+}
+
+int gcd(int a, int b)
+{
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int lcm(int a, int b)
+{
+    return (a*b) / gcd(a, b);    
+}
