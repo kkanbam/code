9fded3fe68106e034fcacfc73f952ec54b88035b
kkanbam <44089681+kkanbam@users.noreply.github.com>
2018-10-15 14:49:05 +0900

    Update 10430.cpp

--- BOJ/10430.cpp
+++ BOJ/10430.cpp
@@ -1,8 +1,13 @@
 /*
-	20181012
-	10430. 나머지
+        20181012
+        10430. 나머지 #수학
+
+        나머지 정리에서 +, *가 성립하는 규칙을 이용해서 푸는 문제임
+        (a + b) % c = (a % c + b % c) % c
+        (a * b) % c = (a % c * b % c) % c
 */
 #include <iostream>
+
 using namespace std;
 
 int main()
